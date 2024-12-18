f0069e66261b97d1810e79c0f01cb94d436fb0a9
root <root@US-5CD012913B.localdomain>
2024-12-17 18:57:56 -0600

    Refactor: ajuste en testapp.py para corregir lógica python

---
+++ testapp.py
@@ -0,0 +1,11 @@
+import requests
+
+url = 'https://magicloops.dev/api/loop/run/bc433371-1eed-4be9-9a07-35e44fb727be'
+payload = {"request": "generate mock user data"}
+
+response = requests.get(url, json=payload, verify=False)
+responseJson = response.json()
+
+print(f"STATUS: {responseJson['status']}")
+print(f"OUTPUT: {responseJson['loopOutput']}")
+
